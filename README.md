9acb6100284010275d731017d7faf4cc384c1be2
Yohan Prakoso <prakoso.yohan@gmail.com>
2026-08-23 03:12:10 +0800

    Add test_2_line_sensors firmware sketch

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Firmware and project files for the Simple Line Follower Robot series.
 ## Structure
 - `fw/`: Firmware sketches
   - `test_single_light_sensor/`: Single photodiode sensor reading test sketch
+  - `test_2_line_sensors/`: Dual line sensor reading test sketch (A0 and A1)
---
+++ fw/test_2_line_sensors/test_2_line_sensors.ino
@@ -0,0 +1,21 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int valueA0 = analogRead(A0);
+  int valueA1 = analogRead(A1);
+
+  float voltageA0 = valueA0 * (5.0 / 1023.0);
+  float voltageA1 = valueA1 * (5.0 / 1023.0);
+
+  char voltA0Str[6], voltA1Str[6];
+  dtostrf(voltageA0, 4, 2, voltA0Str);
+  dtostrf(voltageA1, 4, 2, voltA1Str);
+
+  char buffer[80];
+  snprintf(buffer, sizeof(buffer), "L (A0): %3d (%s V)\tR (A1): %3d (%s V)", valueA0, voltA0Str, valueA1, voltA1Str);
+  Serial.println(buffer);
+
+  delay(100);
+}
